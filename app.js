157ccf8d710843dcd22382d4753ec2852d6161a5
AyushSGithub24 <ayushgupta712005@gmail.com>
2024-06-22 23:31:37 +0530

    adding functionality

--- app.js
+++ app.js
@@ -1,24 +1,68 @@
 const container = document.querySelector(".container");
+let n = 16, count = 0;
+let colour = "black";
+const boxSize = document.querySelector("#BoxSize");
+const clourPicker = document.querySelector("#clourPicker");
+const clear=document.getElementById("CLEAR");
+const random=document.getElementById("random");
 
-let n = 64, count = 0;
+function createGrid() {
+    for (let index = 0; index < n; index++) {
+        let divRow = document.createElement("div")
+        divRow.className = "row";
+        container.appendChild(divRow)
+        for (let i = 0; i < n; i++) {
+            let divCol = document.createElement("div");
+            divCol.id = count++
+            divCol.className = "coloums"
+            divCol.addEventListener("mouseenter", () => {
+                divCol.style.backgroundColor = colour;
+            })
+            divRow.appendChild(divCol)
+        }
+    }
+}
+function deletegrid() {
+    while (container.childElementCount != 0) {
+        container.removeChild(container.lastElementChild);
+    }
+}
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  
 
-for (let index = 0; index < n; index++) {
-    let divRow = document.createElement("div")
-    divRow.className = "row";
-    container.appendChild(divRow)
-    for (let i = 0; i < n; i++) {
-        let divCol = document.createElement("div");
-        divCol.id = count++
-        divCol.className = "coloums"
-        divCol.addEventListener("mouseenter",()=>{
-            divCol.style.backgroundColor="black";
+// start
+createGrid();
+random.addEventListener("click",()=>{
+    for (let i = 0; i < count; i++) {
+        let k = document.getElementById(i);
+        k.addEventListener("mouseenter", () => {
+            k.style.backgroundColor = "#"+randomIntFromInterval(0,16777216);
         })
-        divRow.appendChild(divCol)
     }
 
-}
+})
+clourPicker.addEventListener("change", () => {
+    colour = clourPicker.value;
+    for (let i = 0; i < count; i++) {
+        let k = document.getElementById(i);
+        k.addEventListener("mouseenter", () => {
+            k.style.backgroundColor = colour;
+        })
+    }
 
+})
+boxSize.addEventListener("change", () => {
+    n = boxSize.value
+    deletegrid();
+    createGrid();
 
+})
+clear.addEventListener("click",()=>{
+    deletegrid();
+    createGrid();
+})
 
 
 
